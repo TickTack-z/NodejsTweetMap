ed6c2ef90842e25f1e5240ed3ca5204480604486
TickTack_z <sxzhou@live.com>
2017-10-16 16:11:27 -0400

    map stream

--- public/js/twitterStream.js
+++ public/js/twitterStream.js
@@ -1,71 +1,135 @@
 function initialize() {
-  //Setup Google Map
-  var myLatlng = new google.maps.LatLng(39.7850,188.4183);
-  var light_grey_style = [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}];
-  var myOptions = {
-    zoom: 3,
-    center: myLatlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    mapTypeControl: true,
-    mapTypeControlOptions: {
-      style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-      position: google.maps.ControlPosition.LEFT_BOTTOM
-    },
-    styles: light_grey_style
-  };
-  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    //Setup Google Map
+    var myLatlng = new google.maps.LatLng(37.090, -95.712);
+    var light_grey_style = [{
+        "featureType": "landscape",
+        "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]
+    }, {
+        "featureType": "poi",
+        "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]
+    }, {
+        "featureType": "road.highway",
+        "stylers": [{"saturation": -100}, {"visibility": "simplified"}]
+    }, {
+        "featureType": "road.arterial",
+        "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]
+    }, {
+        "featureType": "road.local",
+        "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]
+    }, {
+        "featureType": "transit",
+        "stylers": [{"saturation": -100}, {"visibility": "simplified"}]
+    }, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {
+        "featureType": "water",
+        "elementType": "labels",
+        "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]
+    }, {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]
+    }];
+    var myOptions = {
+        zoom: 4,
+        center: myLatlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            position: google.maps.ControlPosition.LEFT_BOTTOM
+        },
+        styles: light_grey_style
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
-  map.addListener('click', function(event) {
-    addMarker(event.latLng);
-  });
-
-  //Setup heat map and link to Twitter array we will append data to
-  var heatmap;
-  var liveTweets = new google.maps.MVCArray();
-
-  document.getElementById("button").addEventListener("click", function(){
-    liveTweets.clear();
-  });
-
-  heatmap = new google.maps.visualization.HeatmapLayer({
-    data: liveTweets,
-    radius: 25
-  });
-  heatmap.setMap(map);
-
-  if(io !== undefined) {
-    alert(123);
-    var socket = io.connect('/');
-    // This listens on the "twitter-steam" channel and data is
-    // received everytime a new tweet is receieved.
-    socket.on('twitter-stream', function (data) {
-
-      //Add tweet to the heat map array.
-      var tweetLocation = new google.maps.LatLng(data.lng,data.lat);
-      liveTweets.push(tweetLocation);
-
-      //Flash a dot onto the map quickly
-      var image = "css/small-dot-icon.png";
-      var marker = new google.maps.Marker({
-        position: tweetLocation,
-        map: map,
-        icon: image
-      });
-      setTimeout(function(){
-        marker.setMap(null);
-      },600);
+    var marker, circle;
+    map.addListener('click', function (event) {
+        addMarker(event.latLng);
+        //clear old markers and show new markers
+        //need some code here
 
     });
 
-    socket.on("connected", function(r) {
-      socket.emit("start tweets");
+    function addMarker(location) {
+        if (marker) {
+            marker.setMap(null);
+        }
+        if (circle) {
+            circle.setMap(null);
+        }
+        marker = new google.maps.Marker({
+            position: location,
+            map: map
+        });
+        circle = new google.maps.Circle({
+            map: map,
+            radius: 160930 * 5,    // 1000 miles in metres
+            fillColor: '#AA0000'
+        });
+        circle.bindTo('center', marker, 'position');
+    }
+
+    markers_inst_list = [];
+    var markers = [['London Eye, London', 51.503454, -0.119562], ['Palace of Westminster, London', 51.499633, -0.124755]];
+    var infoWindowContent = [
+        ['London Eye, London'],
+        ['Palace of Westminster, London']
+    ];
+    var infoWindow = new google.maps.InfoWindow(),
+        i;
+
+    for (i = 0; i < markers.length; i++) {
+        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+        markers_inst_list.push(new google.maps.Marker({
+            position: position,
+            map: map,
+            title: markers[i][0]
+        }));
+
+        // Allow each marker to have an info window
+        google.maps.event.addListener(markers_inst_list[markers_inst_list.length - 1], 'click', (function (marker, i) {
+            return function () {
+                infoWindow.setContent(markers[i][0]);
+                infoWindow.open(map, marker);
+            }
+        })(marker, i));
+    }
+
+    document.getElementById("button").addEventListener("click", function () {
+        for (var i = 0; i < markers_inst_list.length; i++) {
+            markers_inst_list[i].setMap(null);
+        }
+        markers_inst_list.length = 0;
     });
-  }
+
+    if (io !== undefined) {
+        var socket = io.connect('/');
+        // This listens on the "twitter-steam" channel and data is
+        // received everytime a new tweet is receieved.
+        socket.on('twitter-stream', function (data) {
+            var tweetLocation = {lat: data.coordinates.coordinates[1], lng: data.coordinates.coordinates[0]};
+
+            markers_inst_list.push(new google.maps.Marker({
+                position: tweetLocation,
+                map: map,
+                title: data.text
+            }));
+
+            google.maps.event.addListener(markers_inst_list[markers_inst_list.length - 1], 'click', (function (marker, i) {
+                return function () {
+                    infoWindow.setContent(data.text);
+                    infowindow.setPosition(marker.getPosition());
+                    infoWindow.open(map, marker);
+                }
+            })(marker, i));
+
+            setTimeout(function () {
+                marker.setMap(null);
+            }, 600);
+        });
+
+        socket.on("connected", function (r) {
+            socket.emit("start tweets");
+        });
+    }
 }
 
-function addMarker(location) {
-    var marker = new google.maps.Marker({
-        position: location,
-        map: map
-    });
-}
--- server.js
+++ server.js
@@ -1,47 +1,52 @@
-//Setup web server and socket
-var twitter = require('twitter'),
+var twitter = require('twit'),
     express = require('express'),
+    bodyParser = require('body-parser'),
     app = express(),
     http = require('http'),
     server = http.createServer(app),
     io = require('socket.io').listen(server);
-    key_word='';
-
 
 //Setup twitter stream api
 var twit = new twitter({
   consumer_key: 'kDoDjz2IZbOkLrzmNF5mrfTgr',
   consumer_secret: '3KuoFjLbHtWOQc2Q586kqjSDV9CNhzrZ1X7S8uUf2IXKBJG7Xv',
-  access_token_key: '494922433-Yih18h5VMiqqXra6kJWb5t8I5F187PHBjPwGM1uN',
+  access_token: '494922433-Yih18h5VMiqqXra6kJWb5t8I5F187PHBjPwGM1uN',
   access_token_secret: '4l4z4JHHeazVMD0xla9XStG9IbWGWn4MYqQ2AzEalwBco'
 });
 
+//Use the default port (for beanstalk) or default to 8081 locally
+server.listen(process.env.PORT || 8081);
+
+//Setup routing for app
 app.use(express.static(__dirname + '/public'));
 
-app.listen(8081);
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
+// parse application/json
+app.use(bodyParser.json())
+
+var stream=twit.stream('statuses/filter', {track:['love','football'],language:'en',location:'-180,-90,180,90'});
 app.get('/getJson', function (req, res) {
-    key_word=req.query["selectpicker"];
-    console.log(key_word);
+        key_word=req.query["selectpicker"];
+        console.log(key_word);
 });
 
-//Create web sockets connection.
 io.sockets.on('connection', function (socket) {
   socket.on("start tweets", function() {
-    if(stream === null) {
-        client.stream('statuses/filter', {track: 'javascript',language:'en',location:'-180,-90,180,90'}, function(stream) {
-            stream.on('data', function(event) {
-                console.log(event && event.text);
-                socket.emit('twitter-stream', outputPoint);
-            });
+    stream.on('tweet', function(data) {
+        if (data.coordinates){
+            if (data.coordinates !== null){
+                console.log(data.text);
+                socket.emit('twitter-stream', data);
+            }
+        }
+    });
 
-            stream.on('error', function(error) {
-                throw error;
-            });
-        });
-    }
+    stream.on('error', function(error) {
+        throw error;
+    });
   });
-  // Emits signal to the client telling them that the
-  // they are connected and can start receiving Tweets
   socket.emit("connected");
 });
+
