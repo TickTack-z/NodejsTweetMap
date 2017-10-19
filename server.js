3a4b6054b3b96787b32bc70010113e36383154d5
TickTack_z <sxzhou@live.com>
2017-10-18 20:16:53 -0400

    TweetsWithFilterWithoutDistance

--- server.js
+++ server.js
@@ -27,18 +27,76 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-var stream=twit.stream('statuses/filter', {track:['love','football',"food"],language:'en',location:'-180,-90,180,90'});
-var key_word="love";
-app.get('/getJson', function (req, res) {
-    key_word=req.query["selectpicker"];
-    console.log(key_word);
-    request('https://search-map-7uq7g47ycnptvveydkyp4lsuca.us-east-1.es.amazonaws.com/twitter/', function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print json
-    });
-});
+app.get("/getJson",function () { return;});
 
+//var track=['love','drink',"food","day","sleep","eat","think","hate","care","study"];
+//var stream=twit.stream('statuses/filter', {track:track,language:'en',location:'-180,-90,180,90'});
+var key_word="love";
+io.sockets.on('connection', function (socket) {
+    socket.on('clicked', function (value) {
+        key_word=value;
+        console.log("keyword:"+key_word);
+        //get the key_word value
+
+        //request ES result
+        var datas=[];
+        var url='https://search-map-7uq7g47ycnptvveydkyp4lsuca.us-east-1.es.amazonaws.com/tweet/_search?size=10000&q=text:'+key_word+'&pretty';
+        request( url,function (error, response, body) {
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            var body2=JSON.parse(body)["hits"];
+            body2=body2.hits;
+            console.log("data num:"+body2.length);
+            for (var i = 0; i < body2.length; i++) {
+                var temp=[body2[i]._source["text"],body2[i]._source["location"][0],body2[i]._source["location"][1]];
+                datas.push(temp);
+            }
+            socket.emit("twitter-stream", datas);
+        });
+    });
+    socket.on('distance', function(latLng){
+        console.log(latLng);
+        var lat=latLng[0];
+        var lng=latLng[1];
+
+        //request ES result
+        var datas=[];
+        var query= {
+            "query": {
+                "bool" : {
+                    "must" : {
+                        "match_all" : {}
+                    },
+                    "filter" : {
+                        "geo_distance" : {
+                            "distance" : "200km",
+                            "location" : {
+                                "lat" : 40,
+                                "lon" : -70
+                            }
+                        }
+                    }
+                }
+            }
+        };
+        var query = require('querystring').stringify(query);
+        var url='https://search-map-7uq7g47ycnptvveydkyp4lsuca.us-east-1.es.amazonaws.com/tweet/_search?pretty';
+        request( url+query ,function (error, response, body) {
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            var body2=JSON.parse(body)["hits"];
+            body2=body2.hits;
+            console.log("data num:"+body2.length);
+            for (var i = 0; i < body2.length; i++) {
+                var temp=[body2[i]._source["text"],body2[i]._source["location"][0],body2[i]._source["location"][1]];
+                datas.push(temp);
+            }
+            socket.emit("twitter-stream", datas);
+        });
+    });
+
+});
+/*
 io.sockets.on('connection', function (socket) {
   socket.on("start tweets", function() {
     stream.on('tweet', function(data) {
@@ -57,3 +115,4 @@ io.sockets.on('connection', function (socket) {
   socket.emit("connected");
 });
 
+*/
